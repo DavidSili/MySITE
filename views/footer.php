e98af082aeb6f20e75367e8ec1ca4f442e7f7271
DavidSili <agapetos@gmail.com>
2016-07-20 20:12:59 +0200

    Ubacio sam tracker i promenio jednu sliku

--- views/footer.php
+++ views/footer.php
@@ -27,3 +27,16 @@
         <img src="images/ico_mail.gif" alt="e-mail" />
     </a>
 </div>
+<!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+    _Hasync.push(['Histats.start', '1,3528016,4,0,0,0,00010000']);
+    _Hasync.push(['Histats.fasi', '1']);
+    _Hasync.push(['Histats.track_hits', '']);
+    (function() {
+        var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+        hs.src = ('//s10.histats.com/js15_as.js');
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+    })();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?3528016&101" alt="" border="0"></a></noscript>
+<!-- Histats.com  END  -->
+
